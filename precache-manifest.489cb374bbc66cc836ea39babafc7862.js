11367aaf0b92c9d404a683ff3cc7b8c05a6cd310
Alex R <areniak147@hotmail.com>
2019-03-06 10:17:55 -0500

    Updates

---
+++ precache-manifest.489cb374bbc66cc836ea39babafc7862.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "8a385cdf6fa15d110664",
+    "url": "/clicky-game/static/js/main.8a385cdf.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clicky-game/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "ca4affe90770f8c54561999e01662ac0",
+    "url": "/clicky-game/index.html"
+  }
+];
